ade46d8fa19752574e0c2b3542a917e4402e4999
roman <romamarysyk@gmail.com>
2026-06-23 03:14:03 +0300

    Initial Commit

---
+++ notifications_meme/notifications_memeApp.swift
@@ -0,0 +1,17 @@
+//
+//  notifications_memeApp.swift
+//  notifications_meme
+//
+//  Created by Roman on 23.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct notifications_memeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
